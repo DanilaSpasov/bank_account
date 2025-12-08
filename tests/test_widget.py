638c9c239d76ec54ac9b6ac31fdf7be5f2aaafa2
Danila Spasov <spasov2000@mail.ru>
2025-12-08 22:28:33 +0300

    small fixes with linters and update README.md

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,22 +1,30 @@
 import pytest
-from src.widget import mask_account_card, get_date
+
+from src.widget import get_date
+from src.widget import mask_account_card
 
 
-@pytest.mark.parametrize("input_data, expected", [
-    ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
-    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-    ("Счет 73654108430135874305", "Счет **4305"),
-    ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-])
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+    ],
+)
 def test_mask_account_card(input_data, expected):
     """Тестирует корректную работу функции mask_account_card с разными входными данными."""
     assert mask_account_card(input_data) == expected
 
 
-@pytest.mark.parametrize("date_str, expected", [
-    ("2024-03-11T02:26:18.671407", "11.03.2024"),
-    ("2019-07-03T18:35:29.512364", "03.07.2019"),
-])
+@pytest.mark.parametrize(
+    "date_str, expected",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("2019-07-03T18:35:29.512364", "03.07.2019"),
+    ],
+)
 def test_get_date(date_str, expected):
     """Тестирует преобразование даты."""
     assert get_date(date_str) == expected
