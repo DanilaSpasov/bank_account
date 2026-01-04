24553cf8f3687325dda4c8108e0e9fea78436f12
Danila Spasov <spasov2000@mail.ru>
2026-01-04 16:18:41 +0300

    add decorators.py and log decorator function

---
+++ src/decorators.py
@@ -0,0 +1,46 @@
+import functools
+from datetime import datetime
+from typing import Optional
+
+
+
+def log(filename: Optional[str] = None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = datetime.now()
+            error = None
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                error = e
+                end_time = datetime.now()
+                log_msg = (
+                    f"{func.__name__} -> ОШИБКА!\n"
+                    f"Тип ошибки: {type(error).__name__}\n"
+                    f"Сообщение: {error}\n"
+                    f"Вводные данные функции: {args}, {kwargs}\n"
+                    f"Время ошибки - {end_time}"
+                )
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(log_msg + "\n")
+                else:
+                    print(log_msg)
+                raise
+            end_time = datetime.now()
+            duration = end_time - start_time
+            if error is None:
+                log_msg = (
+                    f"{func.__name__} -> OK\n"
+                    f"(Время начала работы функции - {start_time}, время окончания выполнения функции - {end_time})\n"
+                    f"Длительность выполнения функции - {duration}"
+                )
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(log_msg + "\n")
+                else:
+                    print(log_msg)
+            return result
+        return wrapper
+    return decorator
