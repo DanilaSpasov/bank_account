659eb6bf9813f195f6a512da6578d2ed34024b3d
Danila Spasov <spasov2000@mail.ru>
2026-01-21 23:33:09 +0300

    add utils.py and json_convertation func

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def json_convertation(file_path) -> list:
+    if not os.path.exists(file_path):
+        return []
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+            if not isinstance(data, list):
+                return []
+
+            return data
+
+    except json.JSONDecodeError:
+        return []
