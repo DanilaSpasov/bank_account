21d94bc6e62516ed1e05ae574481397684183073
Danila Spasov <spasov2000@mail.ru>
2026-01-26 19:42:55 +0300

    update README.md and small fixes

--- src/external_api.py
+++ src/external_api.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 
 
 def transaction_amount(transaction_list:list) -> float:
+    """Функция, которая принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях,
+    тип данных — float. Если транзакция была в USD или EUR,
+    происходит обращение к внешнему API для получения текущего курса валют и конвертации суммы операции в рубли."""
     for transaction in transaction_list:
         from_currency = transaction["operationAmount"]["currency"]["code"]
         if from_currency == "RUB":
