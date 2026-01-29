87df2b7cbd5fa2456067cb809560b107878390b5
Danila Spasov <spasov2000@mail.ru>
2026-01-29 21:02:19 +0300

    modify utils.py with logging

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,28 @@
 import json
+import logging
 import os
 
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('../logs/utils.log', mode='w')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
 def json_convertation(file_path) -> list:
-    """функция, которая принимает на вход путь до JSON-файла
+    """Функция, которая принимает на вход путь до JSON-файла
     и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
     if not os.path.exists(file_path):
+        logger.warning("Путь к файлу не найден, возвращается пустой список.")
         return []
     try:
+        logger.info("Возвращение списка словарей с данными о финансовых транзакциях.")
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             return data if isinstance(data, list) else []
     except (json.JSONDecodeError, OSError):
+        logger.error("Ошибка!", exc_info=True)
         return []
