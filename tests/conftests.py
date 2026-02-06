e2a4daa132a8927b88727434246b5f8f06a0f734
Danila Spasov <spasov2000@mail.ru>
2026-02-06 17:33:55 +0300

    add file_reader.py for opening .xlsx and .csv files

--- tests/conftests.py
+++ tests/conftests.py
@@ -67,3 +67,34 @@ def transaction_generators_list():
         },
     ]
     return transactions
+
+@pytest.fixture
+def test_data_file_reader():
+    """
+    Фикстура возвращает список транзакций для тестов для file_reader
+    """
+    test_data = [
+        {
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210,
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счёт 58803664561298323391",
+            "to": "Счёт 39745660563456619397",
+            "description": "Перевод организации"
+        },
+        {
+            "id": "3598919",
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": 29740,
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту"
+        }
+    ]
+    return test_data
